0bdc20e407efe29a2f309ce7acc618e36184ed5e
Slackluster <29845810+Slackluster@users.noreply.github.com>
2026-06-23 04:44:18 +0200

    Remove comment; we have an issue for that

--- modules/GetInfo.lua
+++ modules/GetInfo.lua
@@ -28,7 +28,6 @@ function app:GetAddonInfo()
 	for i = 1, C_AddOns.GetNumAddOns() do
 		local name, title, notes = C_AddOns.GetAddOnInfo(i)
 		table.insert(app.Info.AddonList, { id = i, iconTexture = C_AddOns.GetAddOnMetadata(i, "IconTexture"), iconAtlas = C_AddOns.GetAddOnMetadata(i, "IconAtlas"), name = name, title = title, notes = notes, version = C_AddOns.GetAddOnMetadata(i, "Version"), interface = C_AddOns.GetAddOnInterfaceVersion(i), author = C_AddOns.GetAddOnMetadata(i, "Author"), category = C_AddOns.GetAddOnMetadata(i, "Category"), dependencies = C_AddOns.GetAddOnDependencies(i) })
-		-- + Addon cache for history
 	end
 end
 
